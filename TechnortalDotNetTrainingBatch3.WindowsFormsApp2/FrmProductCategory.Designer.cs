976dd2231417ab6f1021206de645760b7d4fa013
Min Khant Kyaw <58240917+MinKhantKyaw-design@users.noreply.github.com>
2025-11-10 23:10:29 +0630

    Update UI

--- TechnortalDotNetTrainingBatch3.WindowsFormsApp2/FrmProductCategory.Designer.cs
+++ TechnortalDotNetTrainingBatch3.WindowsFormsApp2/FrmProductCategory.Designer.cs
@@ -29,17 +29,17 @@
         private void InitializeComponent()
         {
             dgvData = new DataGridView();
+            colEdit = new DataGridViewButtonColumn();
+            colDelete = new DataGridViewButtonColumn();
+            colProductCategoryId = new DataGridViewTextBoxColumn();
+            colProductCategoryCode = new DataGridViewTextBoxColumn();
+            colProductCategoryName = new DataGridViewTextBoxColumn();
             btnSave = new Button();
             btnCancel = new Button();
             label1 = new Label();
             label2 = new Label();
             txtProductCategoryCode = new TextBox();
             txtProductCategoryName = new TextBox();
-            colEdit = new DataGridViewButtonColumn();
-            colDelete = new DataGridViewButtonColumn();
-            colProductCategoryId = new DataGridViewTextBoxColumn();
-            colProductCategoryCode = new DataGridViewTextBoxColumn();
-            colProductCategoryName = new DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)dgvData).BeginInit();
             SuspendLayout();
             // 
@@ -48,13 +48,7 @@
             dgvData.AllowUserToAddRows = false;
             dgvData.AllowUserToDeleteRows = false;
             dgvData.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvData.Columns.AddRange(new DataGridViewColumn[] {
-                colEdit,
-                colDelete,
-                colProductCategoryId,
-                colProductCategoryCode,
-                colProductCategoryName
-            });
+            dgvData.Columns.AddRange(new DataGridViewColumn[] { colEdit, colDelete, colProductCategoryId, colProductCategoryCode, colProductCategoryName });
             dgvData.Dock = DockStyle.Bottom;
             dgvData.Location = new Point(0, 180);
             dgvData.Name = "dgvData";
@@ -63,60 +57,6 @@
             dgvData.Size = new Size(700, 250);
             dgvData.TabIndex = 0;
             dgvData.CellContentClick += dgvData_CellContentClick;
-            // 
-            // btnSave
-            // 
-            btnSave.BackColor = Color.DarkSeaGreen;
-            btnSave.Location = new Point(290, 120);
-            btnSave.Name = "btnSave";
-            btnSave.Size = new Size(112, 34);
-            btnSave.TabIndex = 3;
-            btnSave.Text = "&Save";
-            btnSave.UseVisualStyleBackColor = false;
-            btnSave.Click += btnSave_Click;
-            // 
-            // btnCancel
-            // 
-            btnCancel.BackColor = SystemColors.AppWorkspace;
-            btnCancel.Location = new Point(172, 120);
-            btnCancel.Name = "btnCancel";
-            btnCancel.Size = new Size(112, 34);
-            btnCancel.TabIndex = 4;
-            btnCancel.Text = "&Cancel";
-            btnCancel.UseVisualStyleBackColor = false;
-            btnCancel.Click += btnCancel_Click;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(80, 30);
-            label1.Name = "label1";
-            label1.Size = new Size(170, 25);
-            label1.TabIndex = 5;
-            label1.Text = "Product Category Code";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(80, 70);
-            label2.Name = "label2";
-            label2.Size = new Size(177, 25);
-            label2.TabIndex = 6;
-            label2.Text = "Product Category Name";
-            // 
-            // txtProductCategoryCode
-            // 
-            txtProductCategoryCode.Location = new Point(270, 27);
-            txtProductCategoryCode.Name = "txtProductCategoryCode";
-            txtProductCategoryCode.Size = new Size(250, 31);
-            txtProductCategoryCode.TabIndex = 1;
-            // 
-            // txtProductCategoryName
-            // 
-            txtProductCategoryName.Location = new Point(270, 67);
-            txtProductCategoryName.Name = "txtProductCategoryName";
-            txtProductCategoryName.Size = new Size(250, 31);
-            txtProductCategoryName.TabIndex = 2;
             // 
             // colEdit
             // 
@@ -165,6 +105,60 @@
             colProductCategoryName.Name = "colProductCategoryName";
             colProductCategoryName.ReadOnly = true;
             colProductCategoryName.Width = 200;
+            // 
+            // btnSave
+            // 
+            btnSave.BackColor = Color.DarkSeaGreen;
+            btnSave.Location = new Point(290, 120);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(112, 34);
+            btnSave.TabIndex = 3;
+            btnSave.Text = "&Save";
+            btnSave.UseVisualStyleBackColor = false;
+            btnSave.Click += btnSave_Click;
+            // 
+            // btnCancel
+            // 
+            btnCancel.BackColor = SystemColors.AppWorkspace;
+            btnCancel.Location = new Point(172, 120);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(112, 34);
+            btnCancel.TabIndex = 4;
+            btnCancel.Text = "&Cancel";
+            btnCancel.UseVisualStyleBackColor = false;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(69, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(198, 25);
+            label1.TabIndex = 5;
+            label1.Text = "Product Category Code";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(66, 70);
+            label2.Name = "label2";
+            label2.Size = new Size(203, 25);
+            label2.TabIndex = 6;
+            label2.Text = "Product Category Name";
+            // 
+            // txtProductCategoryCode
+            // 
+            txtProductCategoryCode.Location = new Point(278, 27);
+            txtProductCategoryCode.Name = "txtProductCategoryCode";
+            txtProductCategoryCode.Size = new Size(250, 31);
+            txtProductCategoryCode.TabIndex = 1;
+            // 
+            // txtProductCategoryName
+            // 
+            txtProductCategoryName.Location = new Point(278, 67);
+            txtProductCategoryName.Name = "txtProductCategoryName";
+            txtProductCategoryName.Size = new Size(250, 31);
+            txtProductCategoryName.TabIndex = 2;
             // 
             // FrmProductCategory
             // 
